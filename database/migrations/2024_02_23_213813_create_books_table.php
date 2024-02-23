32f66666328ad4a83dbb61918965c25b04857490
N00220782 <N00220782@iadt.ie>
2024-02-23 21:50:00 +0000

    migrations done

---
+++ database/migrations/2024_02_23_213813_create_books_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('price');
+            $table->string('description');
+            $table->string('rating');
+            $table->enum('cover', ['Hardback', 'Paperback']);
+            $table->string('condition');
+            $table->string('image')->default('product.jpg');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
---
+++ database/migrations/2024_02_23_214109_create_wishlist_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('wishlist', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('clothes_id');
+            $table->foreignId('book_id');
+            $table->foreignId('miscellaneous_id');
+
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('clothes_id')->references('id')->on('clothes')->onUpdate('cascade')->onDelete('restrict');
+            $table->foreign('book_id')->references('id')->on('books')->onUpdate('cascade')->onDelete('restrict');
+            $table->foreign('miscellaneous_id')->references('id')->on('miscellaneous')->onUpdate('cascade')->onDelete('restrict');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wishlist');
+    }
+};
