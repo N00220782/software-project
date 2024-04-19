b7383cac4ae2e2a8690db11cf620348cd44dee6b
N00220782 <N00220782@iadt.ie>
2024-04-19 23:27:42 +0100

    fix

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -1,10 +1,11 @@
-<x-app-layout>
-    <x-slot name="header">
+@extends('layouts.myApp')
+    @section('header')
         <h2 class="font-semibold text-xl text-white leading-tight">
             {{ __('Home') }}
         </h2>
-    </x-slot>
+    @endsection
 
+    @section('content')
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
@@ -14,5 +15,4 @@
             </div>
         </div>
     </div>
-
-</x-app-layout>
+    @endsection
